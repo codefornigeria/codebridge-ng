4cc57e19f51d1aea5a568a36aca7686a4483958d
Tobi oladele <tedlade.gmail.com>
2016-11-16 11:34:28 +0100

    Redesign

--- js/app.js
+++ js/app.js
@@ -1,21 +1,11 @@
 $(function() {
-    $('.apply-btn').on("click", function() {
-        $('.apply').show();
-    });
+	$('.hamburger').click(function() {
+        $('.mobile-nav').show(); 
+        $('.logo').hide(); 
+ 	});
 
-    $('.close').on("click", function() {
-        $('.apply').fadeOut();
-    });
-
-    $(".contact-header").typed({
-        strings: ["<b>Hello.^1000</b> How can we be of help ? "],
-        contentType: 'html',
-        typeSpeed: 30,
-        backDelay: 750,
-        showCursor: false,
-        callback: function(){ 
-        	$('.address').show();
-        }
-    });
- });   
-
+	$('.close-nav').click(function() {
+        $('.mobile-nav').hide(); 
+        $('.logo').fadeIn(); 
+ 	});
+});
